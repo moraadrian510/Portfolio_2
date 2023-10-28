bbfadb9e097eb7431071c11e6d2084a87c7d5b2d
Adrian Mora <105446015+moraadrian510@users.noreply.github.com>
2023-10-27 19:59:14 -0700

    submit is working

--- portfolio/src/components/Contact.js
+++ portfolio/src/components/Contact.js
@@ -1,28 +1,46 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser"
 
 
 function Contact() {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_wi99hip', 'template_gia5vva', form.current, 'hz2zDXRAQScEM3sua')
+      .then((result) => {
+          console.log(result.text);
+        form.current.reset();
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
   return (
     <div className="contact-form-container">
       <div className="contact-text">Contact Me</div>
       
-      <form className="contact-form">
+      <form ref={form} onSubmit={sendEmail}className="contact-form" >
         <div className="form-field">
           <label htmlFor="name">Name:</label>
-          <input type="text" id="name" name="name" />
+          <input type="text" id="name" name="user_name" />
         </div>
         <div className="form-field">
           <label htmlFor="email">Email:</label>
-          <input type="email" id="email" name="email" />
+          <input type="email" id="email" name="user_email" />
         </div>
         <div className="form-field">
           <label htmlFor="message">Message:</label>
           <textarea id="message" name="message"></textarea>
         </div>
-        <button className="submit-button" type="submit">Submit</button>
+        <button className="submit-button" type="submit" value="send" >Submit</button>
       </form>
     </div>
   );
 }
 
 export default Contact;
+
+
